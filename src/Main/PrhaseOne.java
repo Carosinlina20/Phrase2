d37a565747d6b104466c3013b7d0cc790dde130c
Carol benavides <carolbenavides@MacBook-Air-de-Carol.local>
2021-11-11 12:19:13 +0100

    first commit

---
+++ src/Main/PrhaseOne.java
@@ -0,0 +1,10 @@
+package Main;
+
+public class PrhaseOne {
+
+    public static void main(String[]args) {
+
+        System.out.println("No hemos sido los primeros, pero seremos los mejores");
+    }
+
+}
